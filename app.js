a1f11ab5d892caf698ffe941573976cce14e6f69
ArfyB <arfybundle@gmail.com>
2023-07-23 13:58:16 +0900

    tot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log(window);
-
+console.log("hi");
 // alert();
 // window.alert();
 
@@ -7,3 +7,6 @@ console.log(window);
 // console.log(document);
 
 // console.dir(document);
+
+document.body.children[1].children[0].href = "https://google.com";
+//              <p>         <a>
